220eb921479fe55c55fa9711156c757f99135a07
NoMeLlamoDante <eo.zm24@outlook.com>
2024-11-27 19:00:34 -0600

    continous movement in four directions

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,12 +31,24 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                elif event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = True
+                    
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                elif event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
-                
+                elif event.key == pygame.K_UP:
+                    self.ship.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = False
 
     def _update_screen(self):
         # Update images on the screeen, and flipto the new screen
--- ship.py
+++ ship.py
@@ -16,12 +16,21 @@ class Ship:
         self.rect.centery = self.screen_rect.centery
         
         # Movement flag; start with a ship that's not moving
+        self.moving_left = False
         self.moving_right = False
+        self.moving_up = False
+        self.moving_down = False
     
     def update(self):
         """Update the ship's position based on the moveemnt flag."""
+        if self.moving_left:
+            self.rect.x -= 1
         if self.moving_right:
+            self.rect.x += 1
+        if self.moving_up:
             self.rect.y -= 1
+        if self.moving_down:
+            self.rect.y += 1
     
     def blitme(self):
         """Draw the ship at its curent location."""
