08da0e48c3c878850488defeb98db4dd64e3853c
NoMeLlamoDante <eo.zm24@outlook.com>
2024-11-27 21:43:08 -0600

    update ship movement

--- ship.py
+++ ship.py
@@ -29,13 +29,13 @@ class Ship:
     def update(self):
         """Update the ship's position based on the moveemnt flag."""
         #Update the ship's x value, not the rect
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_up:
+        if self.moving_up and self.rect.top > self.screen_rect.top:
             self.y -= self.settings.ship_speed
-        if self.moving_down:
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
             self.y += self.settings.ship_speed
 
         #Update rect object from self.x and self.y
